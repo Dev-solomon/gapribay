1cca08ab5c123182d526c1295e88cf77c69dfd00
Dev-solomon <ntiasolomon7@gmail.com>
2024-07-04 12:53:49 -0400

    finish

--- api/app.js
+++ api/app.js
@@ -28,6 +28,14 @@ app.use("/api/test", testRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 
+// Main route
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('../client', 'build', 'index.html'));
+  });
+}
+
 app.listen(port, () => {
   console.log(`Server is running on port:  ${port}`);
 });
